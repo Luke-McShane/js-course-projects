02672abcfd84158cf952b291cba3b38884d55a69
Luke-McShane <lukejsmcshane@gmail.com>
2020-06-08 17:20:07 +1000

    Pangram exercise complete

--- mini_projects/03_pangrams.js
+++ mini_projects/03_pangrams.js
@@ -4,11 +4,14 @@
 let alphabet;
 let charIndex;
 
+// This version adds a letter to the pangramCheck array each time a new letter is found in the sentence parameter
+// If pangramCheck isn't the length of the alphabet, then sentence doesn't include all alphabetical letters
 function isPangram(sentence) {
 	let alphabet = 'abcdefghijklmnopqrstuvwxyz';
 	let pangramCheck = [];
 	sentence = sentence.toLowerCase();
 	for (char of sentence) {
+		// indexOf is used because .includes is not supported in IE
 		if (alphabet.indexOf(char) !== -1 && !pangramCheck.includes(char)) {
 			pangramCheck.push(char);
 		}
@@ -16,4 +19,16 @@ function isPangram(sentence) {
 	return pangramCheck.length === 26;
 }
 
-console.log(isPangram('the quick brown fox jumps over the lazy dog'));
+// This function works in a manner opposite to the one above, in that it iterates through the alphabet, checking the sentence
+// parameter that it contains that current letter iterated on
+function isPangramAlt(sentence) {
+	let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	for (let char of alphabet) {
+		if (sentence.toLowerCase().indexOf(char) === -1) return false;
+	}
+	return true;
+}
+
+let sentence = 'the quick brown fox jumps over the lazy dog';
+console.log(isPangram(sentence));
+console.log(isPangramAlt(sentence));
